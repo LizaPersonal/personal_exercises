2781bdda97b1c3aaf74af33e880e11df5ace283d
LizaRocketrip <liza@rocketrip.com>
2018-03-08 22:43:07 -0500

    new exercises

---
+++ LearnHardWay/ex40.py
@@ -0,0 +1,24 @@
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+
+chorus = "Happy birthday to you"
+name = "Happy birthday dear Liza"
+
+happy_bday = Song([chorus,
+                   chorus,
+                   name,
+                   chorus])
+
+bulls_on_parade = Song(["They rally around tha family",
+                        "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
