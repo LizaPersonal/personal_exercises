698cc44183d16b2efa5200ea30f29073841c4408
LizaRocketrip <liza@rocketrip.com>
2018-03-13 19:07:57 -0400

    Update to Budget Exercise

--- Random_Exercises/budget.py
+++ Random_Exercises/budget.py
@@ -4,44 +4,55 @@
  Second Step: Flight constructor gets number_of_legs and returns 500 * numer_of_legs
               Hotel constructor gets number_of_nights and returns 400 * number_of_nights
               Car constructor gets number_of_days and returns 300 * number_of_days """
+from abc import ABCMeta, abstractmethod
 
 
-class Budget:
+class Budget(object, metaclass=ABCMeta):
+
+    @abstractmethod
     def generate(self):
-        return self
+        pass
 
 
 class Flight(Budget):
     def __init__(self, number_of_legs):
         self.price_per_leg = 500
         self.number_of_legs = number_of_legs
-        self.budget = self.price_per_leg * number_of_legs
+
+    def generate(self):
+        return self.price_per_leg * self.number_of_legs
 
 
 class Hotel(Budget):
+
     def __init__(self, number_of_nights):
         self.price_per_night = 400
         self.number_of_nights = number_of_nights
-        self.budget = self.price_per_night * number_of_nights
+
+    def generate(self):
+        return self.price_per_night * self.number_of_nights
 
 
 class Car(Budget):
     def __init__(self, number_of_days):
         self.price_per_day = 300
         self.number_of_days = number_of_days
-        self.budget = self.price_per_day * number_of_days
+        # self.generate = 5
+
+    def generate(self):
+        return self.price_per_day * self.number_of_days
 
 
 if __name__ == '__main__':
 
     number_of_legs = int(input("How many legs is your flight? "))
     flight_budget = Flight(number_of_legs)
-    print(f"Your flight budget is: {flight_budget.generate}\n")
+    print("Your flight budget is: {}\n".format(flight_budget.generate()))
 
     number_of_nights = int(input("How many nights are you staying? "))
     hotel_budget = Hotel(number_of_nights)
-    print(f"Your hotel budget is: {hotel_budget.budget}\n")
+    print("Your hotel budget is: {}\n".format(hotel_budget.generate()))
 
     number_of_days = int(input("How many days are you renting the car for? "))
     car_budget = Car(number_of_days)
-    print(f"Your car budget is: {car_budget.budget}\n")
+    print("Your car budget is: {}\n".formate(car_budget.generate()))
