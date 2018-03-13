d79bf89c752943d1f01a169eb22b8725fbb67c8d
LizaRocketrip <liza@rocketrip.com>
2018-03-13 18:41:31 -0400

    Commit before pushing to public git

--- Random_Exercises/budget.py
+++ Random_Exercises/budget.py
@@ -13,17 +13,23 @@ class Budget:
 
 class Flight(Budget):
     def __init__(self, number_of_legs):
-        self.generate = 500 * number_of_legs
+        self.price_per_leg = 500
+        self.number_of_legs = number_of_legs
+        self.budget = self.price_per_leg * number_of_legs
 
 
 class Hotel(Budget):
     def __init__(self, number_of_nights):
-        self.generate = 400 * number_of_nights
+        self.price_per_night = 400
+        self.number_of_nights = number_of_nights
+        self.budget = self.price_per_night * number_of_nights
 
 
 class Car(Budget):
     def __init__(self, number_of_days):
-        self.generate = 300 * number_of_days
+        self.price_per_day = 300
+        self.number_of_days = number_of_days
+        self.budget = self.price_per_day * number_of_days
 
 
 if __name__ == '__main__':
@@ -34,8 +40,8 @@ if __name__ == '__main__':
 
     number_of_nights = int(input("How many nights are you staying? "))
     hotel_budget = Hotel(number_of_nights)
-    print(f"Your hotel budget is: {hotel_budget.generate}\n")
+    print(f"Your hotel budget is: {hotel_budget.budget}\n")
 
     number_of_days = int(input("How many days are you renting the car for? "))
     car_budget = Car(number_of_days)
-    print(f"Your car budget is: {car_budget.generate}\n")
+    print(f"Your car budget is: {car_budget.budget}\n")
