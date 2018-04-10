99ec7e34c52c0f02390d63068686ce4fa3139050
LizaRocketrip <liza@rocketrip.com>
2018-04-09 22:34:50 -0400

    ex48 parser

--- LearnHardWay/ex48/ex48/parser.py
+++ LearnHardWay/ex48/ex48/parser.py
@@ -43,3 +43,34 @@ def parse_verb(word_list):
     else:
         raise ParserError("Expected a verb next.")
 
+
+def parse_object(word_list):
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+    if next_word == 'noun':
+        return match(word_list, 'noun')
+    elif next_word == 'direction':
+        return match(word_list, 'direction')
+    else:
+        raise ParserError("Expected a noun or direction next.")
+
+
+def parse_subject(word_list):
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+    if next_word == 'noun':
+        return match(word_list, 'noun')
+    elif next_word == 'verb':
+        return ('noun', 'player')
+    else:
+        raise ParserError("Expected a verb next.")
+
+
+def parse_sentence(word_list):
+    subj = parse_subject(word_list)
+    verb = parse_verb(word_list)
+    obj = parse_object(word_list)
+
+    return Sentence(subj, verb, obj)
