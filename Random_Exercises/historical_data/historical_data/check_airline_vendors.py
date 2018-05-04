f461e782ac65aded88729fa29030d5d63fca7477
LizaRocketrip <liza@rocketrip.com>
2018-05-04 16:42:28 -0400

    attempted to break out the code for the database connection.

--- Random_Exercises/historical_data/historical_data/check_airline_vendors.py
+++ Random_Exercises/historical_data/historical_data/check_airline_vendors.py
@@ -1,37 +1,30 @@
 import mysql.connector
-
-
-def connect_to_historical_database():
-    connection = mysql.connector.connect(host='customerhistorical-vpc-enc.cvhe9o57xgm1.us-east-1.rds.amazonaws.com',
-                                         user='readwrite',
-                                         password='analyzerforwriter1234',
-                                         database='historical')
-    cursor = connection.cursor()
-    return connection, cursor
+from python_mysql_connect import connect_to_database
 
 
 def validate_airline_vendor(airline_in_file):
-    connection, cursor = connect_to_historical_database()
-    search_results = _search_airline_database(cursor)
+    historical_db_connection = connect_to_database()
+    cursor = historical_db_connection.cursor()
+    search_results = _search_airline_database(cursor, airline_in_file)
 
     try:
         if search_results is None:
             missing_airline = airline_in_file[0]
             update_vendor_code = _get_new_airline_code(missing_airline)
-            _update_database_with_new_airline(connection, cursor, missing_airline, update_vendor_code)
+            _update_database_with_new_airline(historical_db_connection, cursor, missing_airline, update_vendor_code)
             search_results = update_vendor_code
         else:
             print(airline_in_file[0] + ' ---> ' + str(search_results))
         cursor.close()
-        connection.close()
+        historical_db_connection.close()
         return search_results
     except e:
         print(e)
 
 
-def _search_airline_database(cursor):
+def _search_airline_database(cursor, airline_to_search_for):
     query = "SELECT airline_code FROM airlines WHERE airline_fullname = %s"
-    cursor.execute(query, airline_in_file)
+    cursor.execute(query, airline_to_search_for)
     results = cursor.fetchone()
     return results
 
--- Random_Exercises/historical_data/historical_data/python_mysql_connect.py
+++ Random_Exercises/historical_data/historical_data/python_mysql_connect.py
@@ -2,7 +2,7 @@ from mysql.connector import MySQLConnection, Error
 from python_mysql_dbconfig import read_db_config
 
 
-def connect():
+def connect_to_database():
     """ Connect to MySQL database """
 
     db_config = read_db_config()
@@ -20,3 +20,29 @@ def connect():
         print(error)
 
     return conn
+
+
+# def connect_to_historical_database():
+#
+#     db_config = """host='customerhistorical-vpc-enc.cvhe9o57xgm1.us-east-1.rds.amazonaws.com',
+#                    user='readwrite',
+#                    password='analyzerforwriter1234',
+#                    database='historical'"""
+#
+#     try:
+#         print('Connecting to MySQL database...')
+#         connection = mysql.connector.connect(db_config)
+#
+#         if connection.is_connected():
+#             print('connection established.')
+#         else:
+#             print('connection failed.')
+#
+#     except e:
+#         print(e)
+#
+#     return connection
+
+
+# connect = connect_to_database()
+# connect.close()
