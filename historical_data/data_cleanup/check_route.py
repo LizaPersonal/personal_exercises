faafc9fdba65798bc929281c69068bfbd3df7c34
LizaRocketrip <liza@rocketrip.com>
2018-05-16 17:02:54 -0400

    Small update to the check_route, to only update the file if it is not already using the correct icon already.

--- historical_data/data_cleanup/check_route.py
+++ historical_data/data_cleanup/check_route.py
@@ -12,6 +12,8 @@ def validate_route_icons(route_in_file, destination_icon, connecting_icon, openj
 def _clean_route(route_in_file, icon_in_file, correct_icon):
     """ Take the route and find if the icon is in the route.
         If so, update the route with the correct icon. """
-    if icon_in_file in route_in_file:
-        route_in_file.replace(icon_in_file, correct_icon)
+
+    if icon_in_file != correct_icon:
+        if icon_in_file in route_in_file:
+            route_in_file.replace(icon_in_file, correct_icon)
     return route_in_file
