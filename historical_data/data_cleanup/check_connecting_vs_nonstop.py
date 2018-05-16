10438fee66cbd2ad50d363513ff0061e04e67741
LizaRocketrip <liza@rocketrip.com>
2018-05-16 10:56:15 -0400

    comments for the connecting vs nonstop

--- historical_data/data_cleanup/check_connecting_vs_nonstop.py
+++ historical_data/data_cleanup/check_connecting_vs_nonstop.py
@@ -1,5 +1,7 @@
 
 def nonstop_or_connecting(route_in_file):
+    """ Take in a route and identify if there is an indication for connecting. """
+
     if "-" in route_in_file:
         return "Connecting"
     else:
