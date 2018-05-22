b2b3b13c5346a76474c4706e999d82ef4ab2f15e
LizaRocketrip <liza@rocketrip.com>
2018-05-22 18:35:19 -0400

    small update to tests.

--- historical_data/tests/test_file_reader.py
+++ historical_data/tests/test_file_reader.py
@@ -10,11 +10,13 @@ def test_comparing_headers_with_matching_headers():
     expected: the template headers did not change
     """
 
-    file_header = ["header 1", "header 2"]
-    original_template = {"h1": "header 1", "h2": "header 2"}
-    template_parameter = copy(original_template)
-    compare_headers(file_header, template_parameter)
-    assert template_parameter == original_template
+    original_file_headers = ["header 1", "header 2"]
+    original_template_headers = {"h1": "header 1", "h2": "header 2"}
+    updated_file_headers = copy(original_file_headers)
+    updated_template_headers = copy(original_template_headers)
+    compare_headers(updated_file_headers, updated_template_headers)
+    assert updated_template_headers == original_template_headers
+    assert updated_file_headers == original_file_headers
 
 
 def test_comparing_headers_with_missing_header_from_file():
@@ -24,11 +26,13 @@ def test_comparing_headers_with_missing_header_from_file():
     expected: the template headers update the additional expected headers
     """
 
-    file_header = ["header 1", "header 2"]
-    original_template = {"h1": "header 1", "h2": "header 2", "h3": ""}
-    final_template = {"h1": "header 1", "h2": "header 2", "h3": "h3"}
-    compare_headers(file_header, original_template)
-    assert final_template == original_template
+    original_file_headers = ["header 1", "header 2"]
+    original_template_headers = {"h1": "header 1", "h2": "header 2", "h3": ""}
+    updated_file_headers = ["header 1", "header 2", "h3"]
+    updated_template_headers = {"h1": "header 1", "h2": "header 2", "h3": "h3"}
+    compare_headers(original_file_headers, original_template_headers)
+    assert updated_template_headers == original_template_headers
+    assert original_file_headers == updated_file_headers
 
 
     # def test_valid_file(self):
