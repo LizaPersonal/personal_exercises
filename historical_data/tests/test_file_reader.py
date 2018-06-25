9dfe0af36b86c04eb01b65ebf73a03d6229ebcc5
LizaRocketrip <liza@rocketrip.com>
2018-06-25 18:44:11 -0400

    no new updates, just a refresh of the push

--- historical_data/tests/test_file_reader.py
+++ historical_data/tests/test_file_reader.py
@@ -44,3 +44,20 @@ class ComparerTests(TestCase):
         assert updated_template_headers == original_template_headers
         assert original_file_headers == updated_file_headers
 
+    def test_comparing_headers_with_extra_header_in_file(self):
+        """
+        method name: compare_headers
+        scenario: when the headers from the file have fewer than the headers needed from the template
+        expected: the template headers update the additional expected headers
+        """
+
+        self.headers_provider_mock.get_headers.return_value = ["h1", "h2"]
+
+        original_file_headers = ["header 1", "header 2", "header 3"]
+        original_template_headers = {"h1": "header 1", "h2": "header 2"}
+        updated_file_headers = copy(original_file_headers)
+        updated_template_headers = copy(original_template_headers)
+        compare_headers(original_file_headers, original_template_headers, self.headers_provider_mock)
+        assert updated_template_headers == original_template_headers
+        assert original_file_headers == updated_file_headers
+
