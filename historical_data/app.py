db384d706471a4177af13ce137e4693eb742c47b
LizaRocketrip <liza@rocketrip.com>
2018-06-27 12:09:48 -0400

    updated to app and html settings

--- historical_data/app.py
+++ historical_data/app.py
@@ -11,46 +11,57 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+
 @app.route("/", methods=["GET", "POST"])
 def upload_historical_data():
+    return render_template("import_data.html")
 
-    error = ""
-
-    try:
-        if request.method == "POST":
-
-            upload_file = request.files['raw_historical_data_csv']
-            tmc = request.form.get('tmc_name')
-            travel = request.form.get('travel_mode')
-            client = request.form['organization_name']
-
-            if upload_file.filename == '':
-                error = "No file selected."
-                return render_template("import_data.html", error=error)
-
-            elif upload_file and allowed_file(upload_file.filename):
-                print(upload_file.filename)
-                print(tmc)
-                print(travel)
-                print(client)
-                return render_template("review_data.html")
-                # return "The {} has been uploaded to {} for {}, based on the {} template" \
-                #     .format(upload_file.filename, travel, client, tmc)
-
-            elif not allowed_file(upload_file.filename):
-                error = "That file type is not allowed."
-                return render_template("import_data.html", error=error)
-
-        return render_template("import_data.html")
-
-    except Exception as e:
-        print(e)
-        return render_template("import_data.html", error=error)
+    # error = ""
+    #
+    # try:
+    #     if request.method == "POST":
 
 
-@app.route('/file/')
+            # if upload_file.filename == '':
+            #     error = "No file selected."
+            #     return render_template("import_data.html", error=error)
+            #
+            # elif upload_file and allowed_file(upload_file.filename):
+            #     return render_template("review_data.html")
+            #     # return "The {} has been uploaded to {} for {}, based on the {} template" \
+            #     #     .format(upload_file.filename, travel, client, tmc)
+            #
+            # elif not allowed_file(upload_file.filename):
+            #     error = "That file type is not allowed."
+            #     return render_template("import_data.html", error=error)
+
+        # return render_template("import_data.html")
+
+    # except Exception as e:
+    #     print(e)
+    #     return render_template("import_data.html", error=error)
+
+
+@app.route("/file", methods=["GET", "POST"])
 def file_sample():
-    return render_template("review_data.html")
+    if request.method == "POST":
+        upload_file = request.files['raw_historical_data_csv']
+        tmc = request.form.get('tmc_name')
+        travel = request.form.get('travel_mode')
+        client = request.form['organization_name']
+
+        print(upload_file.filename)
+        print(tmc)
+        print(travel)
+        print(client)
+
+        data = upload_file.read()
+        print(data)
+
+        file_view = request.form
+        return render_template("review_data.html", result=file_view, data=data)
+    # if request.method == "GET":
+    #     return render_template("review_data.html")
 
 
 if __name__ == "__main__":
