8b5bd434d91db2486dad9d78f00a3f2ff08d9d78
LizaRocketrip <liza@rocketrip.com>
2018-07-20 15:19:56 -0400

    Updates to price formatting to accomodate negatives and blanks cells.

--- historical_data/data_cleanup/check_base_price.py
+++ historical_data/data_cleanup/check_base_price.py
@@ -2,11 +2,8 @@
 def update_base_price(read_file, flight_headers_in_file):
 
     header_to_look_for = flight_headers_in_file["base_price"]
-    row_count = 0
     for row in read_file:
         base_price_in_file = row[header_to_look_for]
-        row_count += 1
-        print(row_count)
         if base_price_in_file == "":
             row["base_price"] = base_price_in_file
         elif base_price_in_file is None:
@@ -19,11 +16,19 @@ def update_base_price(read_file, flight_headers_in_file):
 
 def check_price_format(price):
     last_digit = len(price)-1
-    while not price[0].isdigit():
+    price_is_negative = ""
+    if price[0] in ["(", "-"]:
+        price_is_negative = "-"
+    while not price[0].isdigit() and last_digit != 0:
         price = price[1:]
         last_digit -= 1
-    while not price[last_digit].isdigit():
+    while not price[last_digit].isdigit() and last_digit != 0:
         price = price[:-1]
         last_digit -= 1
-    formatted_price = price.replace(',', '')
+    if price == ' ':
+        formatted_price = 0.00
+    else:
+        if price_is_negative == "-":
+            price = price_is_negative + price
+        formatted_price = price.replace(',', '')
     return formatted_price
--- historical_data/data_cleanup/check_total_price.py
+++ historical_data/data_cleanup/check_total_price.py
@@ -13,7 +13,9 @@ def update_total_price(read_file, flight_headers_in_file):
         taxes_and_fees_in_file = row[header_for_taxes_and_fees]
         if total_price_in_file != "":
             total_price = check_price_format(total_price_in_file)
-        else:
+        elif base_price_in_file != "" and taxes_and_fees_in_file != "":
             total_price = float(base_price_in_file) + float(taxes_and_fees_in_file)
+        else:
+            total_price = 0.00
         row["total_price"] = total_price
     return read_file
