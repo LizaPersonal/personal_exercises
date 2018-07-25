9c563969f53675eff6950e4cefdbc016418b9828
LizaRocketrip <liza@rocketrip.com>
2018-07-25 18:00:49 -0400

    Updates to the State search for hotels, but there is still more to do to update the alias with new options.

--- historical_data/data_cleanup/check_hotel_state.py
+++ historical_data/data_cleanup/check_hotel_state.py
@@ -31,6 +31,8 @@ def _hotel_state_from_db(state_in_file, country_in_file):
                 search_results = _search_by_iso_2_code_without_country(cursor, state_in_file, country_in_file)
                 if search_results is None:
                     search_results = _search_similar_subdivisions(cursor, state_in_file, country_in_file)
+                    if search_results is None:
+                        search_results = _missing_state(cursor, state_in_file, country_in_file)
         return search_results
     except Exception as e:
         print(e)
@@ -92,14 +94,50 @@ def _search_similar_subdivisions(cursor, state_to_search_for, country_id_to_sear
     return results
 
 
-# def _confirm_country(cursor, state_id_to_search_for, country_id_to_search_for):
-#
-#     query = "SELECT country_id " \
-#             "FROM subdivisions " \
-#             "WHERE id =  %s"
-#     cursor.execute(query, state_id_to_search_for)
-#     country_id_result = cursor.fetchone()
-#     if country_id_result[0] == country_id_to_search_for:
-#         return True
-#     else:
-#         return False
+def _missing_state(cursor, missing_state, missing_country):
+
+    country = _find_country_name(cursor, missing_country)
+    country_name = country[0]
+    print(missing_state + " is not an identified state of " + country_name)
+    print("The following states are available in " + country_name + ":")
+    state_options = _suggest_similar_states(cursor, missing_country)
+    _print_suggestions(state_options)
+    selected_state = input("What state should it be mapped to? (please enter the ID) ")
+    return selected_state
+
+
+def _find_country_name(cursor, country_id):
+    query = "SELECT country_formal_name " \
+            "FROM countries " \
+            "WHERE id =  %s"
+    cursor.execute(query, country_id)
+    country_name = cursor.fetchone()
+    return country_name
+
+
+def _suggest_similar_states(cursor, missing_country):
+
+    # results = []
+    query = "SELECT subdivision_name, " \
+            "iso_3166_2_code, " \
+            "id " \
+            "FROM subdivisions " \
+            "WHERE country_id = %s"
+    cursor.execute(query, missing_country)
+    result = cursor.fetchall()
+    return result
+
+
+def _print_suggestions(suggested_states):
+    for suggestion in suggested_states:
+        print(suggestion)
+
+
+def _update_database_with_new_subdivision_alias(connection, cursor, missing_alias, missing_subdivision_id):
+
+    update_query = "INSERT INTO subdivisions_aliases (airline_alias_name, subdivision_id) " \
+                   "VALUES (%s, %s)"
+    data = (missing_alias, missing_subdivision_id)
+    print(data)
+    cursor.execute(update_query, data)
+    connection.commit()
