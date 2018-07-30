1f176747020d397e6f77c56f1c223e970bd131c2
LizaRocketrip <liza@rocketrip.com>
2018-07-30 17:39:54 -0400

    Small adjustment to the price in USD cleanup.

--- historical_data/data_cleanup/check_base_price_usd.py
+++ historical_data/data_cleanup/check_base_price_usd.py
@@ -3,8 +3,5 @@ def update_base_price_usd(read_file):
 
     for row in read_file:
         if row.get("base_price") is not None:
-            if row["exchange_rate"] == 1:
-                row["base_price_usd"] = row["base_price"]
-            else:
-                row["base_price_usd"] = float(row["base_price"]) * float(row["exchange_rate"])
+            row["base_price_usd"] = float(row["base_price"]) * float(row["exchange_rate"])
     return read_file
--- historical_data/data_cleanup/check_total_price_usd.py
+++ historical_data/data_cleanup/check_total_price_usd.py
@@ -2,8 +2,6 @@
 def update_total_price_usd(read_file):
 
     for row in read_file:
-        if row["exchange_rate"] == 1:
-            row["total_price_usd"] = row["total_price"]
-        else:
+        if row.get("total_price") is not None:
             row["total_price_usd"] = float(row["total_price"]) * float(row["exchange_rate"])
     return read_file
